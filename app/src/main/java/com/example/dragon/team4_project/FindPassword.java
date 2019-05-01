a93492097eda0bd5392600713e0a18104b4a7c03
mac <364497547@qq.com>
2019-05-01 15:45:33 +0900

    findpassword layout 추가하고 FindPassword Class도 추가한다

---
+++ app/src/main/java/com/example/dragon/team4_project/FindPassword.java
@@ -0,0 +1,19 @@
+package com.example.dragon.team4_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FindPassword extends Activity {
+
+
+    public static final String KOREAN_FONT = "font/koreanfont1.TTF";
+    public static final String KOREAN_FONT_2 = "font/koreanfont2.TTF";
+    public static final String ENGLISH_FONT = "font/englishfont1.TTF";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.findpassword1);
+    }
+
+}
