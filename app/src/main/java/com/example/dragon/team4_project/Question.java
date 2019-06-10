d8c27fe419e7eedf953baa28cac7a2b4c21cbdc5
ncw <nacheolwon>
2019-06-10 12:41:15 +0900

    Question.xml 수정 Question.java 수정

--- app/src/main/java/com/example/dragon/team4_project/Question.java
+++ app/src/main/java/com/example/dragon/team4_project/Question.java
@@ -1,13 +1,20 @@
 package com.example.dragon.team4_project;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -22,256 +29,142 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Question extends Activity implements View.OnClickListener,
-        MediaPlayer.OnCompletionListener {
-    private static final int REC_STOP = 0;
-    private static final int RECORDING = 1;
-    private static final int PLAY_STOP = 0;
-    private static final int PLAYING = 1;
-    private static final int PLAY_PAUSE = 2;
+public class Question extends Activity {
+    MediaRecorder recorder;
+    String filename;
 
-    private MediaRecorder mRecorder = null;
-    private MediaPlayer mPlayer = null;
-    private int mRecState = REC_STOP;
-    private int mPlayerState = PLAY_STOP;
-    private SeekBar mRecProgressBar, mPlayProgressBar;
-    private Button mBtnStartRec, mBtnStartPlay;
-    private String mFilePath, mFileName = "test.amr";
-    private TextView mTvPlayMaxPoint;
-
-    private int mCurRecTimeMs = 0;
-    private int mCurProgressTimeDisplay = 0;
-
-    // 녹음시 SeekBar처리
-    Handler mProgressHandler = new Handler() {
-        public void handleMessage(Message msg) {
-            mCurRecTimeMs = mCurRecTimeMs + 100;
-            mCurProgressTimeDisplay = mCurProgressTimeDisplay + 100;
-
-            // 녹음시간이 음수이면 정지버튼을 눌러 정지시켰음을 의미하므로
-            // SeekBar는 그대로 정지시키고 레코더를 정지시킨다.
-            if (mCurRecTimeMs < 0) {
-            }
-            // 녹음시간이 아직 최대녹음제한시간보다 작으면 녹음중이라는 의미이므로
-            // SeekBar의 위치를 옮겨주고 0.1초 후에 다시 체크하도록 한다.
-            else if (mCurRecTimeMs < 60000) {
-                mRecProgressBar.setProgress(mCurProgressTimeDisplay);
-                mProgressHandler.sendEmptyMessageDelayed(0, 100);
-            }
-            // 녹음시간이 최대 녹음제한 시간보다 크면 녹음을 정지 시킨다.
-            else {
-                mBtnStartRecOnClick();
-            }
-        }
-    };
-
-    // 재생시 SeekBar 처리
-    Handler mProgressHandler2 = new Handler() {
-        public void handleMessage(Message msg) {
-            if (mPlayer == null)
-                return;
-
-            try {
-                if (mPlayer.isPlaying()) {
-                    mPlayProgressBar.setProgress(mPlayer.getCurrentPosition());
-                    mProgressHandler2.sendEmptyMessageDelayed(0, 100);
-                } else {
-                    mPlayer.release();
-                    mPlayer = null;
-
-                    updateUI();
-                }
-            } catch (IllegalStateException e) {
-            } catch (Exception e) {
-            }
-        }
-    };
+    MediaPlayer player;
+    int position = 0; // 다시 시작 기능을 위한 현재 재생 위치 확인 변수
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question);
 
-        // SD카드에 디렉토리를 만든다.
-        mFilePath = SunUtil.makeDir("progress_recorder");
+        permissionCheck();
 
+        File sdcard = Environment.getExternalStorageDirectory();
+        File file = new File(sdcard, "recorded.mp4");
+        filename = file.getAbsolutePath();
+        Log.d("Question", "저장할 파일 명 : " + filename);
 
-        mBtnStartRec = (Button) findViewById(R.id.btnStartRec);
-        mBtnStartPlay = (Button) findViewById(R.id.btnStartPlay);
-        mRecProgressBar = (SeekBar) findViewById(R.id.recProgressBar);
-        mPlayProgressBar = (SeekBar) findViewById(R.id.playProgressBar);
-        mTvPlayMaxPoint = (TextView) findViewById(R.id.tvPlayMaxPoint);
-
-        mBtnStartRec.setOnClickListener(this);
-        mBtnStartPlay.setOnClickListener(this);
-    }
-
-    // 버튼의 OnClick 이벤트 리스너
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnStartRec:
-                mBtnStartRecOnClick();
-                break;
-            case R.id.btnStartPlay:
-                mBtnStartPlayOnClick();
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void mBtnStartRecOnClick() {
-        if (mRecState == REC_STOP) {
-            mRecState = RECORDING;
-            startRec();
-            updateUI();
-        } else if (mRecState == RECORDING) {
-            mRecState = REC_STOP;
-            stopRec();
-            updateUI();
-        }
-    }
-
-    // 녹음시작
-    @SuppressLint("WrongConstant")
-    private void startRec() {
-        mCurRecTimeMs = 0;
-        mCurProgressTimeDisplay = 0;
-
-        // SeekBar의 상태를 0.1초후 체크 시작
-        mProgressHandler.sendEmptyMessageDelayed(0, 100);
-
-        if (mRecorder == null) {
-            mRecorder = new MediaRecorder();
-            mRecorder.reset();
-        } else {
-            mRecorder.reset();
-        }
-
-        try {
-            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.RAW_AMR);
-            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-            mRecorder.setOutputFile(mFilePath + mFileName);
-            mRecorder.prepare();
-            mRecorder.start();
-        } catch (IllegalStateException e) {
-            Toast.makeText(this, "IllegalStateException", 1).show();
-        } catch (IOException e) {
-            Toast.makeText(this, "IOException", 1).show();
-        }
-    }
-
-    // 녹음정지
-    private void stopRec() {
-        try {
-            mRecorder.stop();
-        } catch (Exception e) {
-        } finally {
-            mRecorder.release();
-            mRecorder = null;
-        }
-
-        mCurRecTimeMs = -999;
-        // SeekBar의 상태를 즉시 체크
-        mProgressHandler.sendEmptyMessageDelayed(0, 0);
-    }
-
-    private void mBtnStartPlayOnClick() {
-        if (mPlayerState == PLAY_STOP) {
-            mPlayerState = PLAYING;
-            startPlay();
-            updateUI();
-        } else if (mPlayerState == PLAYING) {
-            mPlayerState = PLAY_STOP;
-            stopPlay();
-            updateUI();
-        }
-    }
-
-    // 재생 시작
-    private void startPlay() {
-        // 미디어 플레이어 생성
-        if (mPlayer == null)
-            mPlayer = new MediaPlayer();
-        else
-            mPlayer.reset();
-
-        mPlayer.setOnCompletionListener(this);
-
-        String fullFilePath = mFilePath + mFileName;
-        Log.v("ProgressRecorder", "녹음파일명 ==========> " + fullFilePath);
-
-        try {
-            mPlayer.setDataSource(fullFilePath);
-            mPlayer.prepare();
-            int point = mPlayer.getDuration();
-            mPlayProgressBar.setMax(point);
-
-            int maxMinPoint = point / 1000 / 60;
-            int maxSecPoint = (point / 1000) % 60;
-            String maxMinPointStr = "";
-            String maxSecPointStr = "";
-
-            if (maxMinPoint < 10)
-                maxMinPointStr = "0" + maxMinPoint + ":";
-            else
-                maxMinPointStr = maxMinPoint + ":";
-
-            if (maxSecPoint < 10)
-                maxSecPointStr = "0" + maxSecPoint;
-            else
-                maxSecPointStr = String.valueOf(maxSecPoint);
-
-            mTvPlayMaxPoint.setText(maxMinPointStr + maxSecPointStr);
-        } catch (Exception e) {
-            Log.v("ProgressRecorder", "미디어 플레이어 Prepare Error ==========> " + e);
-        }
-
-        if (mPlayerState == PLAYING) {
-            mPlayProgressBar.setProgress(0);
-
-            try {
-                // SeekBar의 상태를 0.1초마다 체크
-                mProgressHandler2.sendEmptyMessageDelayed(0, 100);
-                mPlayer.start();
-            } catch (Exception e) {
-                Toast.makeText(this, "error : " + e.getMessage(), 0).show();
+        findViewById(R.id.btnStartPlay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playAudio();
             }
+        });
+
+        findViewById(R.id.btnPausePlay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pauseAudio();
+            }
+        });
+
+
+        findViewById(R.id.btnStartRec).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recordAudio();
+            }
+        });
+
+        findViewById(R.id.btnStopRec).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopRecording();
+            }
+        });
+    }
+
+    private void recordAudio() {
+        recorder = new MediaRecorder();
+
+        /* 그대로 저장하면 용량이 크다.
+         * 프레임 : 한 순간의 음성이 들어오면, 음성을 바이트 단위로 전부 저장하는 것
+         * 초당 15프레임 이라면 보통 8K(8000바이트) 정도가 한순간에 저장됨
+         * 따라서 용량이 크므로, 압축할 필요가 있음 */
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC); // 어디에서 음성 데이터를 받을 것인지
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4); // 압축 형식 설정
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+
+        recorder.setOutputFile(filename);
+
+        try {
+            recorder.prepare();
+            recorder.start();
+
+            Toast.makeText(this, "녹음 시작됨.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    private void stopPlay() {
-        // 재생을 중지하고
-        mPlayer.stop();
-        mPlayer.release();
-        mPlayer = null;
-        mPlayProgressBar.setProgress(0);
-
-        // 즉시 SeekBar 메세지 핸들러를 호출한다.
-        mProgressHandler2.sendEmptyMessageDelayed(0, 0);
+    private void stopRecording() {
+        if (recorder != null) {
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+            Toast.makeText(this, "녹음 중지됨.", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public void onCompletion(MediaPlayer mp) {
-        mPlayerState = PLAY_STOP; // 재생이 종료됨
+    private void playAudio() {
+        try {
+            closePlayer();
 
-        // 재생이 종료되면 즉시 SeekBar 메세지 핸들러를 호출한다.
-        mProgressHandler2.sendEmptyMessageDelayed(0, 0);
+            player = new MediaPlayer();
+            player.setDataSource(filename);
+            player.prepare();
+            player.start();
+
+            Toast.makeText(this, "재생 시작됨.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void updateUI() {
-        if (mRecState == REC_STOP) {
-            mBtnStartRec.setText("Rec");
-            mRecProgressBar.setProgress(0);
-        } else if (mRecState == RECORDING)
-            mBtnStartRec.setText("Stop");
+    private void pauseAudio() {
+        if (player != null) {
+            position = player.getCurrentPosition();
+            player.pause();
 
-        if (mPlayerState == PLAY_STOP) {
-            mBtnStartPlay.setText("Play");
-            mPlayProgressBar.setProgress(0);
-        } else if (mPlayerState == PLAYING)
-            mBtnStartPlay.setText("Stop");
+            Toast.makeText(this, "일시정지됨.", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    private void resumeAudio() {
+        if (player != null && !player.isPlaying()) {
+            player.seekTo(position);
+            player.start();
+
+            Toast.makeText(this, "재시작됨.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void stopAudio() {
+        if (player != null && player.isPlaying()) {
+            player.stop();
+
+            Toast.makeText(this, "중지됨.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void closePlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+
+    public void permissionCheck(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, 1);
+        }
+    }
+
+
 }
-
 
